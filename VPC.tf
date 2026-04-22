23e72f211b7bee4d64fa007fb158b8bdfc8327ae
test_repo <rizwan.dude4u@gmail.com>
2026-04-22 21:45:55 +0530

    RDS added

--- VPC.tf
+++ VPC.tf
@@ -219,6 +219,26 @@ esource "aws_autoscaling_group" "public_web_asg" {
   }
 }
 
+# RDS Instance
 
+resource "aws_db_instance" "mydb" {
+  identifier              = "mydb-instance"
+  allocated_storage       = 20
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  username                = "admin"
+  password                = "MySecurePass123!"
+  db_name                 = "mydb"
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.web_sg.id]
+  skip_final_snapshot     = true
+  multi_az                = false
+  publicly_accessible     = true
+
+  tags = {
+    Name = "my-rds-instance"
+  }
+}
 
 
